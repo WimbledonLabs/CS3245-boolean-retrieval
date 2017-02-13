21e7a463a583702e8f71efe16908c9d1464125a4
Stephen Molyneaux <shmolyne@uwaterloo.ca>
2017-02-13 14:50:38 +0800

    progress

--- index.py
+++ index.py
@@ -4,7 +4,56 @@ import os
 import argparse
 import nltk
 
+import file
+
 from collections import defaultdict
+
+def numSkips(count):
+    return floor(count**0.5)
+
+def deserialize(posting, file):
+    word, length, offset = posting
+    skip_count = numSkips(length)
+
+    file.seek(offset)
+
+    for i in range(skip_count + length):
+        if i % (skip_count + 1) == 0:
+            # This is the skip pointer
+            i = file.read(2)
+            i = i[0] << 8 | i[1]
+
+
+            if something:
+                # Take the pointer
+                file.seek(2*skip_count, io.SEEK_CUR)
+        else:
+            yield file.read(2)
+            # file.seek(2, io.SEEK_CUR)
+
+
+def serialize(index, postings, file):
+    for word, ids in index.items():
+        length = len(ids)
+        skip_count = numSkips(length)
+
+        postings[word] = (length, file.tell())
+
+        for i, id in enumerate(ids):
+            if i % skip_count == 0:
+                file.write(bytes([ids[i+skip_count] >> 8,
+                                  ids[i+skip_count] & 255]))
+            file.write(bytes([id >> 8, id & 255]))
+
+
+
+'''
+[ 4sk, 0,   1,  2,  3,
+  8sk  4,   5,  6,  7,
+ 12sk  8,   9, 10, 11,
+ 15sk  12, 13, 14]
+'''
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--source", required=True)
