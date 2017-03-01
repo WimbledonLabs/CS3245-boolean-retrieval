e6990bd5f271ed437a872f6d82e1cd9a52f9a335
Stephen Molyneaux <shmolyne@uwaterloo.ca>
2017-03-01 11:46:40 +0800

    Working w/o skiplists

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 
 from serde import serialize, deserialize
 
+stemmer = nltk.stem.PorterStemmer()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--source", required=True)
@@ -15,29 +16,25 @@ parser.add_argument("-d", "--dictionary", required=True)
 parser.add_argument("-p", "--postings", required=True)
 
 args = parser.parse_args()
-
 document_dir = args.source
 
 index = defaultdict(set)
-postings = {}
-
-stemmer = nltk.stem.PorterStemmer()
 
 for document_name in os.listdir(document_dir):
     with open(os.path.join(document_dir, document_name)) as document:
+        doc_id = int(document_name)
+        index["ALL_DOCS"].add(doc_id)
         for sentence in nltk.sent_tokenize(document.read().lower()):
             for word in nltk.word_tokenize(sentence):
-                index[stemmer.stem(word)].add(document_name)
+                index[stemmer.stem(word)].add(doc_id)
 
-dict_file = open(args.dictionary, 'wb')
-postings_file = open(args.postings, 'wb')
+dictionary = defaultdict(tuple)
 
-for word, docs in index.items():
-    s = serialize(sorted(list(docs)))
-    postings[word] = (len(docs), len(s), dict_file.tell())
-    dict_file.write(s)
+with open(args.postings, 'wb') as postings_file:
+    for word, docs in index.items():
+        s = serialize(sorted(list(docs)))
+        dictionary[word] = (len(docs), len(s), postings_file.tell())
+        postings_file.write(s)
 
-postings_file.write(serialize(postings))
-
-dict_file.close()
-postings_file.close()
+with open(args.dictionary, 'wb') as dict_file:
+    dict_file.write(serialize(dictionary))
--- search.py
+++ search.py
@@ -9,32 +9,68 @@ from serde import serialize, deserialize
 
 import re
 
-"""
-def parse_query_re(query):
-    # Since there are no nested brackets, this is a regular grammar, which
-    # means that we can parse it with just a regex :D :D :D
-    terms = re.split(r'(\([a-z ]*\))', query, flags=re.IGNORECASE)
+TERM_TOTAL = 1000
+stemmer = nltk.stem.PorterStemmer()
 
-    return terms
+# TBD, this should be a super-awesome skip list
+ALL_DOCS = []
 
+def mergeUnion(*doc_lists):
+    result = doc_lists[0]
+    for child in doc_lists[1:]:
+        a = result
+        b = child
+        result = []
 
-def parse_query(query):
-    out = []
-    op = []
-    tokens = re.split(r'(\([a-z ]*\))', query, flags=re.IGNORECASE)
-    for token in tokens:
-        if token == "AND":
-            pass
-        elif token == "OR":
-            pass
-        elif token == "NOT":
-            pass
+        cursor_a = 0
+        cursor_b = 0
 
-        if token.startswith('('):
-            val = parse_query(token[1:-1])
-        else:
-            val = token
-"""
+        while cursor_a < len(a) or cursor_b < len(b):
+            if cursor_a == len(a):
+                # We are at the end of list a, grab all of the rest of list b
+                result.extend(b[cursor_b:])
+                break
+
+            elif cursor_b == len(b):
+                # We are at the end of list b, grab all of the rest of list a
+                result.extend(b[cursor_a:])
+                break
+
+            elif a[cursor_a] < b[cursor_b]:
+                result.append(a[cursor_a])
+                cursor_a += 1
+            elif a[cursor_a] > b[cursor_b]:
+                result.append(b[cursor_b])
+                cursor_b += 1
+            elif a[cursor_a] == b[cursor_b]:
+                result.append(a[cursor_a])
+                cursor_a += 1
+                cursor_b += 1
+    return result
+
+def mergeIntersection(*doc_lists):
+    result = doc_lists[0]
+    for child in doc_lists[1:]:
+        a = result
+        b = child
+        result = []
+
+        cursor_a = 0
+        cursor_b = 0
+
+        while cursor_a < len(a) and cursor_b < len(b):
+            if a[cursor_a] < b[cursor_b]:
+                cursor_a += 1
+            elif a[cursor_a] > b[cursor_b]:
+                cursor_b += 1
+            elif a[cursor_a] == b[cursor_b]:
+                result.append(a[cursor_a])
+                cursor_a += 1
+                cursor_b += 1
+    return result
+
+def invert(docs):
+    return list(doc for doc in ALL_DOCS if doc not in docs)
 
 class Lexer():
     def __init__(self, query):
@@ -57,19 +93,46 @@ class Lexer():
 
 class Expr():
     def __init__(self, *children):
-        self.children = children
+        self.children = list(children)
 
     def __repr__(self):
         return "%s%s" % (self.__class__.__name__, tuple(self.children))
 
 class Not(Expr):
-    pass
+    def compute(self, dictionary, postings_file):
+        return invert(self.children[0].compute(dictionary, postings_file))
+
+    def __len__(self):
+        return TERM_TOTAL - len(self.children[0])
 
 class And(Expr):
-    pass
+    def compute(self, dict_file, postings):
+        inputs = [c.compute(dict_file, postings) for c in self.children]
+
+        # It's best to sort by length first since that reduces
+        # the number of passes through the lists
+        inputs = sorted(inputs, key=lambda x: len(x))
+
+        return mergeIntersection(*inputs)
+
+    def __len__(self):
+        # Worst case estimate is that they're all the same
+        return max(len(c) for c in self.children)
 
 class Or(Expr):
-    pass
+    def compute(self, dict_file, postings):
+        # It's best to sort by length first since that reduces  the number of
+        # passes through the lists (this isn't as useful compared to the AND
+        # operator, but it should still make a difference, considering the case
+        # where you start with one very long list, and merge with a bunch of
+        # smaller ones)
+        inputs = [c.compute(dict_file, postings) for c in self.children]
+        inputs = sorted(inputs, key=lambda x: len(x))
+        return mergeUnion(*inputs)
+
+    def __len__(self):
+        # Worst case estimate is that they're all disjoint
+        return sum(len(c) for c in self.children)
 
 class Word():
     def __init__(self, word):
@@ -77,6 +140,18 @@ class Word():
 
     def __repr__(self):
         return repr(self.word)
+
+    def compute(self, dictionary, postings_file):
+        key = stemmer.stem(self.word)
+        if key in dictionary:
+            count, size, pos = dictionary[key]
+            postings_file.seek(pos)
+            return deserialize(postings_file.read(size))
+        return []
+
+    def __len__(self):
+        # Worst case estimate is that they're all disjoint
+        return sum(len(c) for c in self.children)
 
 
 def parse_query(query):
@@ -86,14 +161,18 @@ def parse_query(query):
 
 def expr():
     e = term()
-    while lexer.consume("OR"):
+    if lexer.consume("OR"):
         e = Or(e, term())
+        while lexer.consume("OR"):
+            e.children.append(term())
     return e
 
 def term():
     e = factor()
-    while lexer.consume("AND"):
+    if lexer.consume("AND"):
         e = And(e, factor())
+        while lexer.consume("AND"):
+            e.children.append(factor())
     return e
 
 def factor():
@@ -120,17 +199,17 @@ parser.add_argument("-o", "--output", required=True)
 args = parser.parse_args()
 
 postings_file = open(args.postings, 'rb')
-dictionary_file = open(args.dictionary, 'rb')
+dict_file = open(args.dictionary, 'rb')
 query_file = open(args.query)
-postings = deserialize(postings_file.read())
+output = open(args.output, 'w')
+
+dictionary = deserialize(dict_file.read())
+
+count, size, pos = dictionary["ALL_DOCS"]
+postings_file.seek(pos)
+ALL_DOCS = deserialize(postings_file.read(size))
 
 for query in query_file:
-    '''
-    doc_count, length, pos = postings[word.strip()]
-    dictionary_file.seek(pos)
-    print(deserialize(dictionary_file.read(length)))
-    '''
     query = query.strip()
-    print(query)
-    print(parse_query(query))
-    print('\n\n')
+    result = parse_query(query).compute(dictionary, postings_file)
+    output.write("\n".join(str(i) for i in result) + '\n')
